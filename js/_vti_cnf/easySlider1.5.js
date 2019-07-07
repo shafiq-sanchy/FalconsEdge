2fb823b2b3cf358a413f3c632179d44e4ea9210e
shafiq-sanchy <srplanet7@gmail.com>
2019-07-08 00:07:19 +0600

    uploaded

---
+++ js/_vti_cnf/easySlider1.5.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|04 Nov 2011 21:29:25 -0000
+vti_extenderversion:SR|12.0.0.4518
+vti_author:SR|sanChy-PC\\sanChy
+vti_modifiedby:SR|sanChy-PC\\sanChy
+vti_timecreated:TR|30 Jun 2009 16:03:42 -0000
+vti_backlinkinfo:VX|FalconsEdge/index.html Denver\\ SEO/raw/creatorpro/html/index.html
+vti_nexttolasttimemodified:TW|04 Nov 2011 21:29:10 -0000
+vti_cacheddtm:TX|04 Nov 2011 21:29:25 -0000
+vti_filesize:IR|4587
---
+++ js/_vti_cnf/jquery.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|10 Jun 2009 03:48:32 -0000
+vti_extenderversion:SR|12.0.0.4518
+vti_author:SR|sanChy-PC\\sanChy
+vti_modifiedby:SR|sanChy-PC\\sanChy
+vti_timecreated:TR|10 Jun 2009 03:48:32 -0000
+vti_cacheddtm:TX|10 Jun 2009 03:48:32 -0000
+vti_filesize:IR|57254
+vti_backlinkinfo:VX|Denver\\ SEO/raw/creatorpro/html/contact.html FalconsEdge/index.html Denver\\ SEO/raw/creatorpro/html/index.html
